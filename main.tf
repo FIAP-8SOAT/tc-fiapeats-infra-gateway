160e7ae4ae50c6af47a70cf209cb3ff92e63363e
fabriciofsousa <134175378+fabriciofsousa@users.noreply.github.com>
2025-04-19 00:14:17 -0300

    ft: adding upload lambda api

--- main.tf
+++ main.tf
@@ -74,3 +74,54 @@ resource "aws_apigatewayv2_stage" "default_stage" {
   name        = "$default"
   auto_deploy = true
 }
+
+# --- Novo User Pool do Cognito (para o upload-api) ---
+data "aws_cognito_user_pool" "upload_user_pool" {
+  user_pool_id = "us-east-1_K6TdejMf2"
+}
+
+# --- Novo User Pool Client (audience usado pelo JWT) ---
+data "aws_cognito_user_pool_client" "upload_client" {
+  client_id    = "3s550l9fqaito48eb8tbu2msrt"
+  user_pool_id = data.aws_cognito_user_pool.upload_user_pool.id
+}
+
+# --- Novo Authorizer JWT específico para o /upload ---
+resource "aws_apigatewayv2_authorizer" "upload_cognito_authorizer" {
+  name            = "upload-api-cognito-authorizer"
+  api_id          = aws_apigatewayv2_api.api.id
+  authorizer_type = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+
+  jwt_configuration {
+    audience = [data.aws_cognito_user_pool_client.upload_client.id]
+    issuer   = "https://cognito-idp.us-east-1.amazonaws.com/${data.aws_cognito_user_pool.upload_user_pool.id}"
+  }
+}
+
+# --- Integração do /upload com a Lambda tc-fiap-upload-video ---
+resource "aws_apigatewayv2_integration" "upload_lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:577638369685:function:tc-fiap-upload-video/invocations"
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+# --- Rota POST /upload protegida com o novo Authorizer ---
+resource "aws_apigatewayv2_route" "upload_post_route" {
+  api_id             = aws_apigatewayv2_api.api.id
+  route_key          = "POST /upload"
+  target             = "integrations/${aws_apigatewayv2_integration.upload_lambda_integration.id}"
+  authorizer_id      = aws_apigatewayv2_authorizer.upload_cognito_authorizer.id
+  authorization_type = "JWT"
+}
+
+# --- Permissão para API Gateway invocar a nova Lambda ---
+resource "aws_lambda_permission" "upload_apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvokeUpload"
+  action        = "lambda:InvokeFunction"
+  function_name = "tc-fiap-upload-video"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
